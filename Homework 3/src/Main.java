04588a7cf5c67129db0281349d4e1639f0adccea
GeorgetaV <148208360+GeorgetaV@users.noreply.github.com>
2023-12-03 23:42:40 +0200

    Homework3

---
+++ Homework 3/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] arg) {
+
+
+        String str = new String("I study Basic Java!");
+
+        printString(str);
+
+    }
+
+    public static void printString(String str) {
+        System.out.println(str);
+        System.out.println(str.charAt(17));
+        System.out.println(str.substring(12, 18));
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+
+    }
+}
